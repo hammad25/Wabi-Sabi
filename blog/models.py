ffd90a87addefd43103357c9d7d689c64151bceb
Hammad <hammad2091504@hotmail.com>
2022-09-18 12:40:12 +0000

    Add blog models and migrate

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,99 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+STATUS = ((0, "Draft"),(1,"Published"))
+QUERY_TYPE = ((0,"Question"),(1,"Contribute"))
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('home')
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    content = models.TextField()
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(
+        User, related_name='blogpost_like', blank=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    query_type = models.CharField(max_length=11, choices=QUERY_TYPE, default=0)
+    email = models.EmailField()
+    textbox = models.TextField()
+
+
+# Profile
+class FavoritePost(models.Model):
+    ''' Model that define the favorites chatroom database design '''
+
+    class Meta:
+        '''Set verbose name'''
+        verbose_name_plural = 'Favorites Blogs'
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                    related_name='favorites_blog')
+    post = models.ForeignKey(Post, on_delete=models.SET_NULL,
+                            null=True, blank=True,)
+
+
+class UserProfile(models.Model):
+    ''' Model that define user profile information '''
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+                                related_name='user_profile')
+    about = models.TextField(max_length=2000, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    make_public = models.BooleanField(default=False)
+
+    def __str__(self):
+        ''' Return the username '''
+        return str(self.user.username)
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    ''' Create or upadte the user profile '''
+    if created:
+        UserProfile.objects.create(user=instance)
+    # If existing user, just save
+    instance.user_profile.save()
+
